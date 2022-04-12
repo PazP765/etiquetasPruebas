ba736a34f8943d0511e8854bbe142d8917d156bf
PazP765 <javieerpazz18gmail.com>
2022-04-12 12:54:43 -0500

    123

---
+++ generarLoteEtiquetas.php
@@ -0,0 +1,430 @@
+                  <!-- ETIQUETAS -->
+                    <!-- ETIQUETA DE BOBINA PARA IMPRESION -->
+                    <div kendo-window="comunicado" style="display: none; overflow-y: none;width:auto !important; height:auto !important;">
+                        <div id="todoelcontenido">  
+
+                            <table width="6.5cm" border=1 style="border-collapse: collapse; font-family: 'Verdana', monospace; font-size: 12px;">
+
+                              <tr>
+
+                                <td colspan=2><img src="/assets/img/bf-logo.png" width="153" height="35" alt=""/></td>
+
+                                <td colspan=2><span style="font-size: 8.5px">PRODUCTO&nbsp;TERMINADO BOBINA/BOLSA PIEZA</span></td>
+
+                              </tr>
+
+                              <tr>
+
+                                <td width="69" colspan=1><b>Código: </b> </td>
+                                <td width="79" colspan=1><label id="code23"></label></td>
+                                <td width="65" colspan=1><b>Orden: </b> </td>
+                                <td width="67" colspan=1><label id="orden23"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="47" colspan=1><b>Producto:</b></td>
+
+                                <td colspan=3><label id="producto23"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><b>Máquina:</b></td>
+                                <td colspan=3><label id="">VISION</label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><b>Turno:</b></td>
+                                <td colspan=3><label id="turno23"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><b>Operador:</b></td>
+                                <td colspan=3><label id="operador23"></label></td>
+
+                              </tr>
+                               <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b><label id="unidad23"></label></b></span></td>
+                                <td colspan=1><b><label id="cantidadA23"></label></b></td>
+                                <td colspan=1><span style="font-size: 12px"><b>Diametro (CM):</b></span></td>
+                                <td colspan=1></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=4><center><span id='qrSwiss23'></span></center></td>
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 8px"><b>PRO - FIR - 21</b></span></td>
+                                <td colspan=1><span style="font-size: 8px"><b>Revisión: 02</b></span></td>
+                                <td colspan=2><span style="font-size: 8px"><b>Fecha 14 ABR 20</b></span></td>
+
+                              </tr>
+                
+
+                            </table> 
+                          
+                        </div>
+                          <a href="javascript:imprSelec('todoelcontenido')">Imprimir</a> 
+                          <a onclick="printGrid()"> IMPRIMIR 2 </a>
+                          <input type="button" value="Aceptar" onclick="location.reload()"/>
+                    </div>   
+                    
+                    <!-- ETIQUETA DE EXTRUSIÓN -->
+                    <div kendo-window="comunicado3" style="display: none; overflow-y: none;width:auto !important; height:auto !important;">
+                        <div id="todoelcontenido3">     
+
+                          <table width="6.5cm" border=1 style="border-collapse: collapse; font-family: 'Verdana', monospace; font-size: 12px;">
+
+                              <tr>
+
+                                <td colspan=2><img src="/assets/img/bf-logo.png" width="153" height="35" alt=""/></td>
+
+                                <td colspan=2><span style="font-size: 8.5px">PRODUCTO&nbsp;TERMINADO&nbsp;EXTRUSIÓN</span></td>
+
+                              </tr>
+
+                              <tr>
+
+                                <td width="67" height="28" colspan=1><b>Código: </b> </td>
+                                <td width="80" colspan=1><label id="code24"></label></td>
+                                <td width="50" colspan=1><span style="font-size: 11px"><b>Orden: </b></span> </td>
+                                <td width="76" colspan=1><label id="orden24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="47" colspan=1><b>Producto:</b></td>
+
+                                <td colspan=3><label id="producto24"></label></td>
+
+                              </tr>
+                              <tr>
+                                <td height="34" colspan=1><b>Operador:</b></td>
+                                <td colspan=3><label id="operador24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="26" colspan=1><b>ANCHO:</b></td>
+                                <td colspan=1><label id="ancho24"></label> cm</td>
+                                <td height="24" colspan=1><b>Turno:</b></td>
+                                <td colspan=1><label id="turno24"></label></td>
+                  
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Calibre:</b></span></td>
+                                <td colspan=1><label id="calibre24"></label> micras</td>
+                                <td colspan=1><span style="font-size: 12px"><b>Fórmula:</b></span></td>
+                                <td colspan=1><label id="formula24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><b><span style="font-size: 12px">Diametro:</span></b></td>
+                                <td colspan=1><label id="diametro24"></label> cm</td>
+                                <td colspan=2 rowspan="5"><center><span id='qrSwiss24'></span></center></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Peso:</b></span></td>
+                                <td colspan=1><label id="cantidadA24"></label><b><label id="unidad24"></label></b></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Tratado de Bobina:</b></span></td>
+                                <td colspan=1><label id="tratado24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Color de Bobina:</b></span></td>
+                                <td colspan=1><label id="colorbobina24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Tipo de Bobina:</b></span></td>
+                                <td colspan=1><label id="tipobobina24"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 8px"><b>PRO - FIR - 22</b></span></td>
+                                <td colspan=1><span style="font-size: 8px"><b>Revisión: 02</b></span></td>
+                                <td colspan=2><span style="font-size: 8px"><b>Fecha: 16 MAR 2021</b></span></td>
+
+                              </tr>
+                
+                
+
+                          </table>
+                        </div>
+                        <a href="javascript:imprSelec('todoelcontenido3')">Imprimir</a> 
+                        
+                        <input type="button" value="Aceptar" onclick="location.reload()"/>
+                    </div>    
+              
+                    <!-- ETIQUETA DE LAMINADO -->
+                    <div kendo-window="comunicado4" style="display: none; overflow-y: none;width:auto !important; height:auto !important;">
+                        <div id="todoelcontenido4">          
+                          <table width="6.5cm" border=1 style="border-collapse: collapse; font-family: 'Verdana', monospace; font-size: 12px;">
+
+                              <tr>
+
+                                <td colspan=2><img src="/assets/img/bf-logo.png" width="153" height="35" alt=""/></td>
+
+                                <td colspan=2><span style="font-size: 8.5px">PRODUCTO&nbsp;TERMINADO&nbsp;BOBINA LAMINADA</span></td>
+
+                              </tr>
+
+                              <tr>
+
+                                <td width="67" height="28" colspan=1><b>Código: </b> </td>
+                                <td width="80" colspan=1><label id="code25"></label></td>
+                                <td width="50" colspan=1><span style="font-size: 11px"><b>Orden: </b></span> </td>
+                                <td width="76" colspan=1><label id="orden25"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="47" colspan=1><b>Producto:</b></td>
+
+                                <td colspan=3><label id="producto25"></label></td>
+
+                              </tr>
+                              <tr>
+                                <td height="34" colspan=1><b>Máquina:</b></td>
+                                <td colspan=3><label id="maquina25"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="26" colspan=1><b>Turno:</b></td>
+                                <td colspan=3><label id="turno25"></label></td>
+                  
+                              </tr>
+                               <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Operador:</b></span></td>
+                                <td colspan=3><label id="operador25"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="51" colspan=1><span style="font-size: 12px"><b><label id="unidad25"></label></b></span></td>
+                                <td colspan=1><label id="cantidadA25"></label></td>
+                                <td colspan=2 rowspan="4"><center><span id='qrSwiss25'></span></center></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="40" colspan=1><span style="font-size: 12px"><b>Hora de Laminado:</b></span></td>
+                                <td colspan=1>22:00</td>
+
+                              </tr>
+                               <tr>
+
+                                <td height="48" colspan=1><b><span style="font-size: 12px">Fecha de Curado:</span></b></td>
+                                <td colspan=1>230921</td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><b><span style="font-size: 12px">Hora de Curado:</span></b></td>
+                                <td colspan=1>22:00</td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 8px"><b>PRO - FIR - 22</b></span></td>
+                                <td colspan=1><span style="font-size: 8px"><b>Revisión: 02</b></span></td>
+                                <td colspan=2><span style="font-size: 8px"><b>Fecha: 14 ABR 2020</b></span></td>
+
+                              </tr>
+
+                          </table>
+              
+
+                        </div> 
+
+                        <a href="javascript:imprSelec('todoelcontenido4')">Imprimir</a> 
+                        
+                        <input type="button" value="Aceptar" onclick="location.reload()"/>
+                    </div>
+
+                    <!-- ETIQUETA DE TARIMA -->
+                    <div kendo-window="comunicadotarima" style="display: none; overflow-y: none;width:auto !important; height:auto !important;">
+                        <div id="todoelcontenido2">   
+                          <!-- ETIQUETA DE TARIMA   -->
+
+              
+                            <table width="6.5cm" border=1 style="border-collapse: collapse; font-family: 'Verdana', monospace; font-size: 12px;">
+
+                              <tr>
+
+                                <td colspan=2><img src="/assets/img/bf-logo.png" width="153" height="35" alt=""/></td>
+
+                                <td colspan=2><span style="font-size: 8.5px">PRODUCTO&nbsp;TERMINADO&nbsp;TARIMA</span></td>
+
+                              </tr>
+
+                              <tr>
+
+                                <td width="67" height="28" colspan=1><b>Código: </b> </td>
+                                <td width="80" colspan=1><label id="codeT"></label></td>
+                                <td width="50" colspan=1><span style="font-size: 11px"><b>Orden: </b></span> </td>
+                                <td width="76" colspan=1><label id="ordenT"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="47" colspan=1><b>Producto:</b></td>
+
+                                <td colspan=3><label id="productofinalT"></label></td>
+
+                              </tr>
+                               <tr>
+
+                                <td height="34" colspan=1><b>Operador:</b></td>
+                                <td colspan=3><label id="operadorT"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td height="26" colspan=1><b>ÁREA:</b></td>
+                                <td colspan=1><label id="etapaT"></label></td>
+                                <td colspan=2 rowspan="6"><center><span id='qrSwissT'></span></center></td>
+                              </tr>
+                              <tr>
+
+                                <td height="24" colspan=1><b>Turno:</b></td>
+                                <td colspan=1><label id="turnoT"></label></td>
+
+                              </tr>
+                               <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Peso Bruto:</b></span></td>
+                                <td colspan=1><label id="pesob2"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Peso Neto:</b></span></td>
+                                <td colspan=1><label id="pesoN"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 12px"><b>Peso de Tarima:</b></span></td>
+                                <td colspan=1><label id="pesot2"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 11px"><b><label id="unidadAT"></label></b></span></td>
+                                <td colspan=1><label id="cantidadAT"></label></td>
+
+                              </tr>
+                              <tr>
+
+                                <td colspan=1><span style="font-size: 8px"><b>PRO - FIR - 21</b></span></td>
+                                <td colspan=1><span style="font-size: 8px"><b>Revisión: 02</b></span></td>
+                                <td colspan=2><span style="font-size: 8px"><b>Fecha 14 ABR 20</b></span></td>
+
+                              </tr>
+                
+                
+
+                           </table>
+                        
+
+                        </div>
+                        <a href="javascript:imprSelec('todoelcontenido2')">Imprimir</a> 
+                        
+                        <input type="button" value="Aceptar" onclick="location.reload()"/>
+                    </div>  
+
+
+
+
+<script>
+$("#qrSwiss24").kendoQRCode({
+  value: resp,
+  size: 150,
+  overlay: {
+      type: "gs1 gr code",
+      height: 20,
+      width: 20
+  }
+});                                                                                                                                                                                                                
+</script>
+
+<script>
+    function imprSelec(historial){
+      var ficha=document.getElementById(historial);
+      var ventimp=window.open(' ','popimpr');
+      ventimp.document.write(ficha.innerHTML);
+      ventimp.document.close();
+      ventimp.print();
+      ventimp.close();
+    }   
+</script>
+
+<script>
+    function printGrid() {
+        var gridElement = $('#todoelcontenido'),
+            printableContent = '',
+            win = window.open('', '', 'width=800, height=500, resizable=1, scrollbars=1'),
+            doc = win.document.open();
+        var htmlStart =
+            '<head>' +
+            '<meta charset="utf-8" />' +
+            '<title>BFSAP - ETIQUETA</title>' +
+            '<link href="https://kendo.cdn.telerik.com/' + kendo.version + '/styles/kendo.common.min.css" rel="stylesheet" /> ' +
+            '<style>' +
+            'html { font: 11pt sans-serif; }' +
+            '.k-grid { border-top-width: 0; }' +
+            '.k-grid, .k-grid-content { height: auto !important; }' +
+            '.k-grid-content { overflow: visible !important; }' +
+            'div.k-grid table { table-layout: auto; width: 100% !important; }' +
+            '.k-grid .k-grid-header th { border-top: 1px solid; }' +
+            '.k-grouping-header, .k-grid-toolbar, .k-grid-pager > .k-link { display: none; }' +
+            // '.k-grid-pager { display: none; }' + // optional: hide the whole pager
+            '</style>' +
+            '</head>' +
+            '<body>';
+        var htmlEnd =
+            '</body>';
+        var gridHeader = gridElement.children('.k-grid-header');
+        if (gridHeader[0]) {
+            var thead = gridHeader.find('thead').clone().addClass('k-grid-header');
+            printableContent = gridElement
+                .clone()
+                .children('.k-grid-header').remove()
+                .end()
+                .children('.k-grid-content')
+                .find('table')
+                .first()
+                .children('tbody').before(thead)
+                .end()
+                .end()
+                .end()
+                .end()[0].outerHTML;
+        } else {
+            printableContent = gridElement.clone()[0].outerHTML;
+        }
+        doc.write(htmlStart + printableContent + htmlEnd);
+        doc.close();
+        win.print();
+        doc.close();
+    }
+</script>              
